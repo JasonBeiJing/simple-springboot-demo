659341aa5047d47ce8779634c051b5b8391fba54
unknown <coolday1103@126.com>
2018-04-12 15:49:05 +0800

    haha

---
+++ src/main/java/com/springboot/demo/entity/Attribute.java
@@ -0,0 +1,40 @@
+package com.springboot.demo.entity;
+
+import java.io.Serializable;
+
+public class Attribute implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7581779002779038526L;
+
+	private String key;
+	private Object value;
+	
+	public Attribute() {
+		super();
+	}
+
+	public Attribute(String key, Object value) {
+		super();
+		this.key = key;
+		this.value = value;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public Object getValue() {
+		return value;
+	}
+
+	public void setValue(Object value) {
+		this.value = value;
+	}
+}
---
+++ src/main/java/com/springboot/demo/entity/User.java
@@ -0,0 +1,43 @@
+package com.springboot.demo.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class User implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5885111588950855229L;
+	private Long id;
+	private String name;
+	private List<Attribute> attributes;
+
+	public User() {
+		super();
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Attribute> getAttributes() {
+		return attributes;
+	}
+
+	public void setAttributes(List<Attribute> attributes) {
+		this.attributes = attributes;
+	}
+
+}
